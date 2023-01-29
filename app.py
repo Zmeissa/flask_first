def9534424eabb571ec09c96493f842d5f629859
ZagrebinaN <deira@bk.ru>
2023-01-29 19:01:49 +0300

    add search

--- app.py
+++ app.py
@@ -14,21 +14,25 @@ quotes = [
    {
        "id": 3,
        "author": "Rick Cook",
+       "rating": 5,
        "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
    },
    {
        "id": 5,
        "author": "Waldi Ravens",
+       "rating": 3,
        "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
    },
    {
        "id": 6,
        "author": "Mosher’s Law of Software Engineering",
+       "rating": 5,
        "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
    },
    {
        "id": 8,
        "author": "Yoggi Berra",
+       "rating": 2,
        "text": "В теории, теория и практика неразделимы. На практике это не так."
    }
 ]
@@ -73,18 +77,20 @@ def about():
     return about_me
 
 
-# Практика 1.2 Задания 1-2
+# Практика 1.2 Задания 1-2 + рейтинг
 @app.route("/quotes", methods=["POST"])
 def create_quote():
     new_quote = request.json
     last_quote = quotes[-1]
     new_id = last_quote["id"] + 1
     new_quote["id"] = new_id
+    if 'rating' not in new_quote or not 1 <= new_quote['rating'] <= 5:
+        new_quote["rating"] = 1
     quotes.append(new_quote)
     return new_quote, 201
 
 
-# Задание 4
+# Задание 4 + рейтинг
 @app.route("/quotes/<int:quote_id>", methods=['PUT'])
 def edit_quote(quote_id):
     for quote in quotes:
@@ -94,6 +100,8 @@ def edit_quote(quote_id):
                 quote["author"] = new_data['author']
             if "text" in new_data:
                 quote['text'] = new_data['text']
+            if "rating" in new_data and 1 <= new_data['rating'] <= 5:
+                quote['rating'] = new_data['rating']
             return quote, 200
     return f"Quote with id={quote_id} not found", 404
 
@@ -103,9 +111,27 @@ def edit_quote(quote_id):
 def delete(quote_id):
     for quote in quotes:
         if quote["id"] == quote_id:
-            del quotes[quote_id - 1]
-            return f'Quote with id = {quote_id} is delete', 204
+            quotes.remove(quote)
+            return f"Quote with id = {quote_id} is delete", 200
     return f"Quote with id={quote_id} not found", 404
+
+
+# Задание 2.2
+@app.route('/quotes/search', methods=['GET'])
+def search():
+    args = request.args
+    author = args.get('author', type=str)
+    rating = args.get('rating', default=0, type=int)
+    result = {}
+    for quote in quotes:
+        if quote["author"] == author and quote["rating"] == rating:
+            result = {key: value for key, value in quote.items()}
+        if quote["author"] == author:
+            result = {key: value for key, value in quote.items()}
+        if quote["rating"] == rating:
+            result = {key: value for key, value in quote.items()}
+
+        return result
 
 
 if __name__ == "__main__":
